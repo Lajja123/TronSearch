4318eba70debfa20818b8a7aae6fdcdc91bd2640
Lajja123 <lajja.lamprostech@gmail.com>
2023-09-12 14:30:26 +0530

    search-widget all components added

--- src/pages/widgets.tsx
+++ src/pages/widgets.tsx
@@ -32,40 +32,37 @@ const Widget: React.FC<WidgetProps> = ({ showWidget, setShowWidget }) => {
       className={`search-widget ${showWidget ? "open" : "closed"}`}
       id="search-widget"
     >
-      <div className="widget-content-container">
-        <div className="w-sec1-flex">
-          <div style={{ color: "white" }}>Logo</div>
-          <button className="close-button" onClick={() => handleClose()}>
-            ✖
-          </button>
+      <div className="w-sec1-flex">
+        <div style={{ color: "white" }}>Logo</div>
+        <button className="close-button" onClick={() => handleClose()}>
+          ✖
+        </button>
+      </div>
+      <div>
+        <div className="w-sec2-flex">
+          <input
+            type="text"
+            placeholder=" Enter Address / Txn Hash "
+            onChange={handleSearchButtonClick}
+            className="search-input"
+          />
+          <select>
+            <option value="option1">Select...</option>
+            <option value="option2">Address</option>
+            <option value="option3">Hashvalue</option>
+          </select>
         </div>
         <div>
-          <div className="w-sec2-flex">
-            <input
-              type="text"
-              placeholder=" Enter Address / Txn Hash "
-              onChange={handleSearchButtonClick}
-              className="search-input"
-            />
-            <select>
-              <option value="option1">Select...</option>
-              <option value="option2">Address</option>
-              <option value="option3">Hashvalue</option>
-            </select>
-          </div>
-          <div>
-            <button className="search-button" onClick={handleSearchButtonClick}>
-              Search
-            </button>
-          </div>
+          <button className="search-button" onClick={handleSearchButtonClick}>
+            Search
+          </button>
         </div>
-        {showDefaultWidgets && <CurrentAddressDetails />}
-
-        {selectedOption === "option2" && !showDefaultWidgets && (
-          <AddressWidget />
-        )}
-        {selectedOption === "option3" && !showDefaultWidgets && <HashWidget />}
       </div>
+
+      {showDefaultWidgets && <CurrentAddressDetails />}
+
+      {selectedOption === "option2" && !showDefaultWidgets && <AddressWidget />}
+      {selectedOption === "option3" && !showDefaultWidgets && <HashWidget />}
     </div>
   );
 };
